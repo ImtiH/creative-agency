09c58173d4d8f30950234c763b002fdb2d4c74d7
Imtiaz Anik <writetoimti@gmail.com>
2021-01-13 14:35:52 -0330

    final and completed proeject

---
+++ src/Conponents/Login/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOxyMxroNaTL9F8HP7jyvcUSRrghvmlcY",
+  authDomain: "creative-agency-anik.firebaseapp.com",
+  projectId: "creative-agency-anik",
+  storageBucket: "creative-agency-anik.appspot.com",
+  messagingSenderId: "871392333244",
+  appId: "1:871392333244:web:9febc134cbf89b3b3bafb9"
+};
+export default firebaseConfig;  
